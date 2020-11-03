9dc6df1bf51f304e46cc0bac20efaf95c9487e17
Jonas Claesson <Jonas.Claesson@arbetsformedlingen.se>
2020-11-03 22:16:05 +0100

    Add test data for dk

--- app/src/Lines.elm
+++ app/src/Lines.elm
@@ -125,6 +125,21 @@ seCountry =
     Country "Sweden" seY
 
 
+dkYD : YearData
+dkYD =
+    YearData <| L.map (\v -> 100 + v) (L.range 1 53)
+
+
+dkY : D.Dict Int Year
+dkY =
+    D.fromList
+        [ ( 2000, Year 1234 dkYD )
+        , ( 2000, Year 2234 dkYD )
+        , ( 2000, Year 3234 dkYD )
+        , ( 2000, Year 4234 dkYD )
+        ]
+
+
 dkCountry : Country
 dkCountry =
-    Country "Denmark" seY
+    Country "Denmark" dkY
