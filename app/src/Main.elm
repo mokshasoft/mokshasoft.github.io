a3472e77f4df705a40b0c59b0c7b587bdf19eec7
Jonas Claesson <Jonas.Claesson@arbetsformedlingen.se>
2020-11-03 20:53:38 +0100

    Add license headers

--- app/src/Main.elm
+++ app/src/Main.elm
@@ -1,3 +1,12 @@
+{-
+   Copyright 2020, Mokshasoft AB (mokshasoft.com)
+
+   This software may be distributed and modified according to the terms of
+   the GNU General Public License v3.0. Note that NO WARRANTY is provided.
+   See "LICENSE" for details.
+-}
+
+
 module Main exposing (..)
 
 import Bootstrap.CDN as CDN
