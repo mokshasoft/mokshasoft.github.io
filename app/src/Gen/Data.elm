56a521030ef9aa067d8900fde61cf9849946d2a8
Jonas Claesson <Jonas.Claesson@arbetsformedlingen.se>
2020-11-05 18:59:04 +0100

    Simplify elm data file for easier generation

--- app/src/Gen/Data.elm
+++ app/src/Gen/Data.elm
@@ -26,36 +26,28 @@ countries =
     ]
 
 
-seY : D.Dict Int Year
-seY =
-    D.fromList
-        [ ( 2000, Year 1234 (L.range 1 53) )
-        , ( 2000, Year 2234 (L.range 1 53) )
-        , ( 2000, Year 3234 (L.range 1 53) )
-        , ( 2000, Year 4234 (L.range 1 53) )
-        ]
-
-
 seCountry : Country
 seCountry =
-    Country "Sweden" seY
+    Country "Sweden" <|
+        D.fromList
+            [ ( 2000, Year 1234 (L.range 1 53) )
+            , ( 2000, Year 2234 (L.range 1 53) )
+            , ( 2000, Year 3234 (L.range 1 53) )
+            , ( 2000, Year 4234 (L.range 1 53) )
+            ]
+
+
+dkCountry : Country
+dkCountry =
+    Country "Denmark" <|
+        D.fromList
+            [ ( 2000, Year 1234 dkYD )
+            , ( 2000, Year 2234 dkYD )
+            , ( 2000, Year 3234 dkYD )
+            , ( 2000, Year 4234 dkYD )
+            ]
 
 
 dkYD : List Int
 dkYD =
     L.map (\v -> 100 + v) (L.range 1 53)
-
-
-dkY : D.Dict Int Year
-dkY =
-    D.fromList
-        [ ( 2000, Year 1234 dkYD )
-        , ( 2000, Year 2234 dkYD )
-        , ( 2000, Year 3234 dkYD )
-        , ( 2000, Year 4234 dkYD )
-        ]
-
-
-dkCountry : Country
-dkCountry =
-    Country "Denmark" dkY
