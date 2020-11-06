92d0ed0d3b1ec91efeb55872d51b5e9fd50adb2a
Jonas Claesson <Jonas.Claesson@arbetsformedlingen.se>
2020-11-06 19:04:00 +0100

    Add the Swedish population in an elm file

---
+++ app/src/Gen/Population.elm
@@ -0,0 +1,38 @@
+{-
+   Copyright 2020, Mokshasoft AB (mokshasoft.com)
+
+   This software may be distributed and modified according to the terms of
+   the GNU General Public License v3.0. Note that NO WARRANTY is provided.
+   See "LICENSE" for details.
+-}
+
+
+module Gen.Population exposing (popSweden)
+
+import Dict as D
+
+
+popSweden : D.Dict Int Int
+popSweden =
+    D.fromList
+        [ ( 2000, 8882792 )
+        , ( 2001, 8909128 )
+        , ( 2002, 8940788 )
+        , ( 2003, 8975670 )
+        , ( 2004, 9011392 )
+        , ( 2005, 9047752 )
+        , ( 2006, 9113257 )
+        , ( 2007, 9182927 )
+        , ( 2008, 9256347 )
+        , ( 2009, 9340682 )
+        , ( 2010, 9415570 )
+        , ( 2011, 9482855 )
+        , ( 2012, 9555893 )
+        , ( 2013, 9644864 )
+        , ( 2014, 9747355 )
+        , ( 2015, 9851017 )
+        , ( 2016, 9995153 )
+        , ( 2017, 10120242 )
+        , ( 2018, 10230185 )
+        , ( 2019, 10327589 )
+        ]
