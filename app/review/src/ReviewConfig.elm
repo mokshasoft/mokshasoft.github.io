e4454951e5b7980394468d2ad7c2f7e2f4a5929c
Jonas Claesson <Jonas.Claesson@arbetsformedlingen.se>
2020-11-07 11:04:09 +0100

    Add some elm-review rules
    Run the tool like this:
    $ elm-review

--- app/review/src/ReviewConfig.elm
+++ app/review/src/ReviewConfig.elm
@@ -11,9 +11,24 @@ when inside the directory containing this file.
 
 -}
 
+import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
+import NoUnused.Exports
+import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
+import NoUnused.Variables
 import Review.Rule exposing (Rule)
 
 
 config : List Rule
 config =
-    []
+    [ NoUnused.Variables.rule
+    , NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.Dependencies.rule
+    , NoUnused.Exports.rule
+    , NoUnused.Modules.rule
+    , NoUnused.Parameters.rule
+    , NoUnused.Patterns.rule
+    , NoUnused.Variables.rule
+    ]
