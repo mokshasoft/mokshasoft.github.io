e2815e13168147ae7a7c9565057b4857944fd411
Jonas Claesson <Jonas.Claesson@arbetsformedlingen.se>
2020-11-07 09:32:55 +0100

    Break up Analysis functions in smaller parts to simplify code

--- app/src/Analysis.elm
+++ app/src/Analysis.elm
@@ -42,31 +42,58 @@ type alias ChartInfo =
 
 
 
--- FUNCTIONS
+-- HELPER FUNCTIONS
 
 
-mortality : List ChartInfo -> Int -> List ChartInfo
-mortality cs population =
+{-| Mortality rate expressed in a population of 1000 individuals.
+
+The input List should contain deaths/week.
+
+-}
+mortality : Int -> List ChartInfo -> List ChartInfo
+mortality population cs =
     L.map (\ct -> { ct | y = 1000 * toFloat 52 * ct.y / toFloat population }) cs
 
 
-selectYear : Int -> Country -> List ChartInfo
-selectYear year country =
-    let
-        yearDataM =
-            D.get year country.data
+{-| Transform data to ChartInfo
+-}
+toChartInfo : List Int -> List ChartInfo
+toChartInfo ls =
+    L.map2 (\i d -> ChartInfo (toFloat i) (toFloat d)) (L.range 1 (L.length ls)) ls
 
+
+{-| Select a year from country data.
+-}
+getYearData : Int -> Country -> List ChartInfo
+getYearData year country =
+    let
         data =
-            case yearDataM of
+            case D.get year country.data of
                 Nothing ->
                     []
 
                 Just d ->
                     d.data
+
+        pop =
+            1
     in
-    L.map2 (\i d -> ChartInfo (toFloat i) (toFloat d)) (L.range 1 (L.length data)) data
+    mortality pop (toChartInfo data)
 
 
+{-| Trim of i elements and the end of the ChartInfo.
+-}
+trimData : Int -> List ChartInfo -> List ChartInfo
+trimData i ls =
+    L.take (L.length ls - i) ls
+
+
+
+-- ANALYSIS
+
+
+{-| Display mortality in 2020 with highest mortality of 2000-2019.
+-}
 maxAnalysis : Selection -> Analysis
 maxAnalysis s =
     let
@@ -74,18 +101,12 @@ maxAnalysis s =
             C.getCountry s.country
 
         year =
-            selectYear s.year c
-
-        trimYear =
-            L.take (L.length year - 4) year
-
-        minYear =
-            selectYear 2019 c
+            getYearData s.year c
 
         maxYear =
-            selectYear 2002 c
+            getYearData 2002 c
     in
     Analysis
-        [ LineChart.line Color.black Dots.diamond "2020" <| mortality trimYear 10327589
-        , LineChart.line Color.red Dots.diamond "2002" <| mortality maxYear 8940788
+        [ LineChart.line Color.black Dots.diamond "2020" <| trimData 4 year
+        , LineChart.line Color.red Dots.diamond "2002" maxYear
         ]
